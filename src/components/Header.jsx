ebd8753c136dad1350decf111b1b8e92c1aec307
M Tariq <tariqsyed6610@gmail.com>
2025-01-25 19:55:23 -0800

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
   return (
     <AppBar position="sticky">
       <Toolbar>
-        <Box className={classes.navbar}>
+        <Box className={classes.navbar }>
           <Typography variant="h6" className={classes.logoText}>
             MyLogo
           </Typography>
